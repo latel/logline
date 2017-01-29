5778286b45f9e377d443055672db6b4bad58d424
kevinnwang <kevinnwang@tencent.com>
2017-01-29 15:54:31 +0800

    add makefile

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,7 @@ export default {
     dest: 'dist/' + pkg.name + (process.env.NODE_ENV === 'production' ? '.min' : '') + '.js',
     format: 'umd',
     moduleName: pkg.name.replace(/^\w/, starter => starter.toUpperCase()),
+    sourceMap: true,
     plugins: [
         babel({
             exclude: 'node_modules/**'
@@ -19,7 +20,7 @@ export default {
         (process.env.NODE_ENV === 'production' && uglify()),
         license({
             banner: {
-                file: path.join(__dirname, 'BANNER')
+                file: path.join(__dirname, 'src', 'BANNER')
             },
             thirdParty: {
                 output: path.join(__dirname, 'dist', 'dependencies.txt'),
